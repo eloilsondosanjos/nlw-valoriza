c6602293b95f24c33339371f2757bc7c730245e1
eloilsondosanjos <eloilson.ageedu@gmail.com>
2021-06-23 07:18:28 -0300

    Criado o servidor da aplicação

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,12 @@ import express from "express";
 
 const app = express();
 
+app.get('/test', (req, res) => {
+  return res.send('Olá, você acabou de acessar a rota de teste')
+})
+
+app.post('/teste', (req, res) => {
+  return res.send('Antenção essa rota é de post no teste')
+})
+
 app.listen(3000, () => console.log("Server is running"))
