62fd88cb14707ce51c98fc58c50f12522bceb543
eloilsondosanjos <eloilson.ageedu@gmail.com>
2021-06-23 19:44:46 -0300

    Criado a conexão com o banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,9 @@
+
+import "reflect-metadata";
 import express from "express";
+import "./database";
+
 
 const app = express();
-
-app.get('/test', (req, res) => {
-  return res.send('Olá, você acabou de acessar a rota de teste')
-})
-
-app.post('/teste', (req, res) => {
-  return res.send('Antenção essa rota é de post no teste')
-})
 
 app.listen(3000, () => console.log("Server is running"))
