5bb73099e392320ea9d3877e13442638a79c214a
eloilsondosanjos <eloilson.ageedu@gmail.com>
2021-06-30 21:11:04 -0300

    Implementação da listagem dos users

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { CreateComplimentController } from "./controllers/CreateComplimentContro
 import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { ListTagsController } from "./controllers/ListTagsController";
+import { ListUsersController } from "./controllers/ListUsersController";
 import { ListUsersReceiveComplimentsController } from "./controllers/ListUsersReceiveComplimentsController";
 import { ListUsersSendComplimentsController } from "./controllers/ListUsersSendComplimentsController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
@@ -20,6 +21,7 @@ const listUsersReceiveComplimentsController =
 const listUsersSendComplimentsController =
   new ListUsersSendComplimentsController();
 const listTagsController = new ListTagsController();
+const listUsersController = new ListUsersController();
 
 router.post(
   "/tags",
@@ -30,6 +32,12 @@ router.post(
 
 router.get("/tags", ensureAuthenticated, listTagsController.handle);
 
+router.get(
+  "/users",
+  ensureAuthenticated,
+  ensureAdmin,
+  listUsersController.handle
+);
 router.post("/users", createUserController.handle);
 router.post("/login", authenticateUserController.handle);
 router.post(
